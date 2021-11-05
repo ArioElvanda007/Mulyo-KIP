87ead1f882841416423afe40a8f852f0fdaceb25
mulyo.saputra <mulyo.saputra@gmail.com>
2021-11-05 14:48:41 +0700

    push

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -57,9 +57,9 @@
       <!-- Logo -->
       <a href="../../index2.html" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>K</b>IP</span>
+        <span class="logo-mini"><b>M</b>DH</span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>Kelman Infra Pratama</b></span>
+        <span class="logo-lg"><b>MINARTA DUTAHUTAMA</b></span>
       </a>
       <!-- Header Navbar: style can be found in header.less -->
       <nav class="navbar navbar-static-top">
